a68503680cd8eec39a65a019dcf21a36967a37c7
Mykola Chaikovskyi <nickchaywork@gmail.com>
2020-12-04 19:27:21 +0200

    Добавлен вход на добавление криптовалюты

--- app/src/main/java/live/nickp0is0n/cryptotracker/ui/CryptoListActivity.kt
+++ app/src/main/java/live/nickp0is0n/cryptotracker/ui/CryptoListActivity.kt
@@ -1,10 +1,15 @@
 package live.nickp0is0n.cryptotracker.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import drewcarlson.coingecko.CoinGeckoService
 import kotlinx.android.synthetic.main.activity_crypto_list.*
+import kotlinx.coroutines.launch
 import live.nickp0is0n.cryptotracker.R
 import live.nickp0is0n.cryptotracker.adapter.CryptoAdapter
 import live.nickp0is0n.cryptotracker.models.CryptoCurrency
@@ -17,6 +22,25 @@ class CryptoListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_crypto_list)
         initRecyclerView()
         loadCurrencyList()
+    }
+
+    fun onAddCryptocurrencyButtonClick(view: View) {
+        clProgressBar.isVisible = true
+        lifecycleScope.launch {
+            val coinList = getCoinList()
+            val intent = Intent(this@CryptoListActivity, AddCurrencyActivity::class.java)
+            intent.putExtra("coinList", coinList)
+            startActivity(intent)
+        }
+    }
+
+    private suspend fun getCoinList(): ArrayList<String> {
+        val service = CoinGeckoService()
+        val coinList = arrayListOf<String>()
+        service.getCoinList().forEach {
+            coinList.add(it.name)
+        }
+        return coinList
     }
 
     private fun initRecyclerView() {
